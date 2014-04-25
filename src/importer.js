3dce59c3939ad9cdbd595fd1e702a6dd111cc905
Dave Turissini <davidairborne@gmail.com>
2014-04-24 19:33:02 -0700

    Now exposing similar artists

--- src/importer.js
+++ src/importer.js
@@ -1,4 +1,4 @@
 var importTracksFromArtist = require('./import_tracks_from_artist');
 
 exports.import = importTracksFromArtist.importTracksFromArtist
-exports.findRelatedArtists = importTracksFromArtist.findAdjacentArtists;
+exports.findSimilarArtists = importTracksFromArtist.findAdjacentArtists;
